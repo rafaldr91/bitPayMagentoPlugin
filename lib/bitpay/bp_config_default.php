b5c83595e8b9075fa29e0d7864846552204645f5
Ryan X. Charles <ryan@bitpay.com>
2013-12-12 15:49:02 -0500

    add ssl config

--- lib/bitpay/bp_config_default.php
+++ lib/bitpay/bp_config_default.php
@@ -4,7 +4,8 @@ $bpconfig_port=443;
 $bpconfig_hostAndPort=$bpconfig_host;
 if ($bpconfig_port!=443)
 	$bpconfig_hostAndPort.=":".$bpconfig_port;
-
+$bpconfig_ssl_verifypeer=1;
+$bpconfig_ssl_verifyhost=2;
 //include custom config overrides if it exists
 if (file_exists('bp_config.php'))
 	require_once 'bp_config.php';
