b5c5b23f88d5cc35dbc7586d1c734f45f49a26cb
Joshua Estes <josh@bitpay.com>
2014-07-02 17:11:08 -0400

    Updated the build.xml file with different way to run phpunit, update phpunit config and added a simple test to test in travis

---
+++ tests/app/code/community/Bitpay/Bitcoins/Block/IframeTest.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * ©2011,2012,2013,2014 BITPAY, INC.
+ * 
+ * Permission is hereby granted to any person obtaining a copy of this software
+ * and associated documentation for use and/or modification in association with
+ * the bitpay.com service.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * 
+ * Bitcoin payment plugin using the bitpay.com service.
+ */
+
+class Bitpay_Bitcoins_Block_IframeTest extends PHPUnit_Framework_TestCase
+{
+
+    public function testSomething()
+    {
+        $this->assertTrue(true);
+    }
+}
