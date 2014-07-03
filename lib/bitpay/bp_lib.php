e8202f1574538042bf738a4d7cfc55bd70d7de8e
Joshua Estes <josh@bitpay.com>
2014-07-03 13:48:20 -0400

    Updates the  variable to stay as a string and not switch from boolean to string

--- lib/bitpay/bp_lib.php
+++ lib/bitpay/bp_lib.php
@@ -25,18 +25,18 @@ require_once 'bp_options.php';
 /**
  * @param string $url
  * @param string $apiKey
- * @param bool   $post
+ * @param string $post
  *
  * @return array
  */
-function bpCurl($url, $apiKey, $post = false)
+function bpCurl($url, $apiKey, $post = null)
 {
     global $bpOptions, $bpconfig;	
 
     $curl   = curl_init($url);
     $length = 0;
 
-    if ($post)
+    if (null !== $post)
     {	
         curl_setopt($curl, CURLOPT_POST, 1);
         curl_setopt($curl, CURLOPT_POSTFIELDS, $post);
