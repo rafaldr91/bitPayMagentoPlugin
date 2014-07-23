66c714d474a4d66c07a07d544ad6254dcc225385
Joshua Estes <josh@bitpay.com>
2014-07-23 16:40:53 -0400

    Small copy edit

--- app/code/community/Bitpay/Bitcoins/Model/Observer.php
+++ app/code/community/Bitpay/Bitcoins/Model/Observer.php
@@ -3,7 +3,7 @@
 /**
  * The MIT License (MIT)
  * 
- * Copyright (c) 2011-2014 BitPay LLC
+ * Copyright (c) 2011-2014 BitPay, Inc.
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
