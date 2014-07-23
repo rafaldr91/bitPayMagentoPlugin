ff0801199b61e83fed4a4d9a0f51239365669518
Joshua Estes <josh@bitpay.com>
2014-07-23 13:25:41 -0400

    A little refactoring

--- app/code/community/Bitpay/Bitcoins/Helper/Data.php
+++ app/code/community/Bitpay/Bitcoins/Helper/Data.php
@@ -77,58 +77,17 @@ class Bitpay_Bitcoins_Helper_Data extends Mage_Core_Helper_Abstract
     {
         $expiredRecords = Mage::getModel('Bitcoins/ipn')->getExpired();
 
-        // Parse each record
         foreach ($expiredRecords as $ipn) {
             $incrementId = $ipn->getOrderId();
             if (empty($incrementId)) {
-                Mage::log(
-                    'Error processing IPN record',
-                    Zend_Log::DEBUG,
-                    self::LOG_FILE
-                );
-                Mage::log(
-                    $ipn->toJson(),
-                    Zend_Log::DEBUG,
-                    self::LOG_FILE
-                );
-                /**
-                 * We have no way to tie this to any magento order so it needs
-                 * to be deleted
-                 */
-                $ipn->delete();
-                Mage::log(
-                    'IPN record deleted from database',
-                    Zend_Log::DEBUG,
-                    self::LOG_FILE
-                );
+                $this->logIpnParseError($ipn);
                 continue;
             }
 
-            // Cancel the order in the system
-            $order      = Mage::getModel('sales/order')->loadByIncrementId($incrementId);
-            $orderState = $order->getState();
-
-            /**
-             * If the order is complete, we do not want to cancel it
-             */
-            $statesWeDontCareAbout = array(
-                Mage_Sales_Model_Order::STATE_CANCELED,
-                Mage_Sales_Model_Order::STATE_CLOSED,
-                Mage_Sales_Model_Order::STATE_COMPLETE,
+            // Cancel the order
+            $this->cancelOrder(
+                Mage::getModel('sales/order')->loadByIncrementId($incrementId)
             );
-            if (!in_array($orderState, $statesWeDontCareAbout)) {
-                $order->setState(
-                    Mage_Sales_Model_Order::STATE_CANCELED,
-                    true,
-                    'BitPay Invoice has expired', // comment
-                    false // notifiy customer?
-                )->save();
-                Mage::log(
-                    sprintf('Order "%s" has been canceled', $order->getIncrementId()),
-                    Zend_Log::DEBUG,
-                    self::LOG_FILE
-                );
-            }
 
             // Delete all IPN records for order id
             Mage::getModel('Bitcoins/ipn')
@@ -139,5 +98,76 @@ class Bitpay_Bitcoins_Helper_Data extends Mage_Core_Helper_Abstract
                 self::LOG_FILE
             );
         }
+    }
+
+    /**
+     * Log error if there is an issue parsing an IPN record
+     *
+     * @param Bitpay_Bitcoins_Model_Ipn $ipn
+     * @param boolean                   $andDelete
+     */
+    private function logIpnParseError(Bitpay_Bitcoins_Model_Ipn $ipn, $andDelete = true)
+    {
+        Mage::log(
+            'Error processing IPN record',
+            Zend_Log::DEBUG,
+            self::LOG_FILE
+        );
+        Mage::log(
+            $ipn->toJson(),
+            Zend_Log::DEBUG,
+            self::LOG_FILE
+        );
+        if ($andDelete) {
+            $ipn->delete();
+        }
+        Mage::log(
+            'IPN record deleted from database',
+            Zend_Log::DEBUG,
+            self::LOG_FILE
+        );
+    }
+
+    /**
+     * This will cancel the order in the magento database, this will return
+     * true if the order was canceled or it will return false if the order
+     * was not updated. For example, if the order is complete, we don't want
+     * to cancel that order so this method would return false.
+     *
+     * @param Mage_Sales_Model_Order
+     *
+     * @return boolean
+     */
+    private function cancelOrder(Mage_Sales_Model_Order $order)
+    {
+        $orderState = $order->getState();
+
+        /**
+         * These order states are useless and can just be skipped over. No
+         * need to cancel an order that is alread canceled.
+         */
+        $statesWeDontCareAbout = array(
+            Mage_Sales_Model_Order::STATE_CANCELED,
+            Mage_Sales_Model_Order::STATE_CLOSED,
+            Mage_Sales_Model_Order::STATE_COMPLETE,
+        );
+
+        if (in_array($orderState, $statesWeDontCareAbout)) {
+            return false;
+        }
+
+        $order->setState(
+            Mage_Sales_Model_Order::STATE_CANCELED,
+            true,
+            'BitPay Invoice has expired', // Comment
+            false // notifiy customer?
+        )->save();
+        Mage::log(
+            sprintf('Order "%s" has been canceled', $order->getIncrementId()),
+            Zend_Log::DEBUG,
+            self::LOG_FILE
+        );
+
+        return true;
     }
 }
