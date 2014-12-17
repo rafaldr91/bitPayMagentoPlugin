f2323c659aa0f307cf0eaff1584a996e2e2b1bd1
Rich Morgan <ionux@users.noreply.github.com>
2014-12-17 14:54:57 -0500

    Ucwords Formatting Fix

--- app/code/community/Bitpay/Core/Model/TransactionSpeed.php
+++ app/code/community/Bitpay/Core/Model/TransactionSpeed.php
@@ -16,9 +16,9 @@ class Bitpay_Core_Model_TransactionSpeed
     public function toOptionArray()
     {
         return array(
-            array('value' => self::SPEED_LOW, 'label'    => Mage::helper('bitpay')->__(self::SPEED_LOW)),
-            array('value' => self::SPEED_MEDIUM, 'label' => Mage::helper('bitpay')->__(self::SPEED_MEDIUM)),
-            array('value' => self::SPEED_HIGH, 'label'   => Mage::helper('bitpay')->__(self::SPEED_HIGH)),
+            array('value' => self::SPEED_LOW, 'label'    => Mage::helper('bitpay')->__(ucwords(self::SPEED_LOW)),
+            array('value' => self::SPEED_MEDIUM, 'label' => Mage::helper('bitpay')->__(ucwords(self::SPEED_MEDIUM)),
+            array('value' => self::SPEED_HIGH, 'label'   => Mage::helper('bitpay')->__(ucwords(self::SPEED_HIGH)),
         );
     }
 }
