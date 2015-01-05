5caa1c396266976ccabfc251fb0a8080d5cabe7a
Rich Morgan <ionux@users.noreply.github.com>
2015-01-05 15:57:57 -0500

    Added namespace backslash

--- app/code/community/Bitpay/Core/Model/Status.php
+++ app/code/community/Bitpay/Core/Model/Status.php
@@ -19,12 +19,12 @@ class Bitpay_Core_Model_Status
     public function toOptionArray()
     {
         return array(
-            array('value' => self::STATUS_NEW, 'label'       => Mage::helper('bitpay')->__(ucwords(self::STATUS_NEW))),
-            array('value' => self::STATUS_PAID, 'label'      => Mage::helper('bitpay')->__(ucwords(self::STATUS_PAID))),
-            array('value' => self::STATUS_CONFIRMED, 'label' => Mage::helper('bitpay')->__(ucwords(self::STATUS_CONFIRMED))),
-            array('value' => self::STATUS_COMPLETE, 'label'  => Mage::helper('bitpay')->__(ucwords(self::STATUS_COMPLETE))),
-            array('value' => self::STATUS_EXPIRED, 'label'   => Mage::helper('bitpay')->__(ucwords(self::STATUS_EXPIRED))),
-            array('value' => self::STATUS_INVALID, 'label'   => Mage::helper('bitpay')->__(ucwords(self::STATUS_INVALID))),
+            array('value' => self::STATUS_NEW, 'label'       => \Mage::helper('bitpay')->__(ucwords(self::STATUS_NEW))),
+            array('value' => self::STATUS_PAID, 'label'      => \Mage::helper('bitpay')->__(ucwords(self::STATUS_PAID))),
+            array('value' => self::STATUS_CONFIRMED, 'label' => \Mage::helper('bitpay')->__(ucwords(self::STATUS_CONFIRMED))),
+            array('value' => self::STATUS_COMPLETE, 'label'  => \Mage::helper('bitpay')->__(ucwords(self::STATUS_COMPLETE))),
+            array('value' => self::STATUS_EXPIRED, 'label'   => \Mage::helper('bitpay')->__(ucwords(self::STATUS_EXPIRED))),
+            array('value' => self::STATUS_INVALID, 'label'   => \Mage::helper('bitpay')->__(ucwords(self::STATUS_INVALID))),
         );
     }
 }
