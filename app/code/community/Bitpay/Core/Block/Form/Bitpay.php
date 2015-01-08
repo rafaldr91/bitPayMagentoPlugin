8b5ea763f35d5aa507231ee61eae46f3fda09b51
Rich Morgan <ionux@users.noreply.github.com>
2015-01-08 17:21:15 -0500

    Added error handling and debugging

--- app/code/community/Bitpay/Core/Block/Form/Bitpay.php
+++ app/code/community/Bitpay/Core/Block/Form/Bitpay.php
@@ -8,7 +8,15 @@ class Bitpay_Core_Block_Form_Bitpay extends Mage_Payment_Block_Form
 {
     protected function _construct()
     {
+        $payment_template = 'bitpay/form/bitpay.phtml';
+
         parent::_construct();
-        $this->setTemplate('bitpay/form/bitpay.phtml');
+
+        if (true === file_exists($payment_template) && true === is_readable($payment_template)) {
+            $this->setTemplate($payment_template);
+        } else {
+            \Mage::helper('bitpay')->debugData('[ERROR] In Bitpay_Core_Block_Form_Bitpay::_construct(): HTML payment template missing or unreadable.');
+            throw new \Exception('In Bitpay_Core_Block_Iframe::getIframeUrl(): HTML payment template missing or unreadable.');
+        }
     }
 }
