7aea57bed0e5b8276a39a44a7c23ec98e328145f
Rich Morgan <ionux@users.noreply.github.com>
2015-01-12 13:28:54 -0500

    Removed template file check
    This was causing an unecessary error during checkout.

--- app/code/community/Bitpay/Core/Block/Form/Bitpay.php
+++ app/code/community/Bitpay/Core/Block/Form/Bitpay.php
@@ -11,12 +11,7 @@ class Bitpay_Core_Block_Form_Bitpay extends Mage_Payment_Block_Form
         $payment_template = 'bitpay/form/bitpay.phtml';
 
         parent::_construct();
-
-        if (true === file_exists($payment_template) && true === is_readable($payment_template)) {
-            $this->setTemplate($payment_template);
-        } else {
-            \Mage::helper('bitpay')->debugData('[ERROR] In Bitpay_Core_Block_Form_Bitpay::_construct(): HTML payment template missing or unreadable.');
-            throw new \Exception('In Bitpay_Core_Block_Iframe::getIframeUrl(): HTML payment template missing or unreadable.');
-        }
+        
+        $this->setTemplate($payment_template);
     }
 }
